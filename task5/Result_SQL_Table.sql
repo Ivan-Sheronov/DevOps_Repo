f872257574f8791385ca2edd2cbba09ee274f082
Ivan <evansgreap@gmail.com>
2021-02-12 16:41:48 +0300

    task5 is done on minimal req-s

---
+++ task5/Result_SQL_Table.sql
@@ -0,0 +1,26 @@
+create table result (
+	Student_ID INT NOT NULL PRIMARY KEY,
+	student_last_name VARCHAR(50) NOT NULL,
+	task1 TEXT,
+	task2 TEXT,
+	task3 TEXT,
+	task4 TEXT
+);
+insert into result (Student_ID, student_last_name, task1, task2, task3, task4) values (1, 'Rajabov', 'pass', 'pass', 'pass', 'pass');
+insert into result (Student_ID, student_last_name, task1, task2, task3, task4) values (2, 'Sivozhelezov', 'pass', 'pass', 'pass', '');
+insert into result (Student_ID, student_last_name, task1, task2, task3, task4) values (3, 'Chesnyuk', 'pass*', 'pass', 'pass*', 'pass*');
+insert into result (Student_ID, student_last_name, task1, task2, task3, task4) values (4, 'Matskevich', 'pass', 'pass', 'not pass', '');
+insert into result (Student_ID, student_last_name, task1, task2, task3, task4) values (5, 'Sheronov', 'pass', 'pass', 'not pass', 'not pass');
+insert into result (Student_ID, student_last_name, task1, task2, task3, task4) values (6, 'Gavrish', 'pass', 'pass', 'not pass', '');
+insert into result (Student_ID, student_last_name, task1, task2, task3, task4) values (7, 'Demyankov', 'pass', 'not pass', 'not pass', 'not pass');
+insert into result (Student_ID, student_last_name, task1, task2, task3, task4) values (8, 'Pehotsky', 'pass', 'pass', 'pass', 'pass');
+insert into result (Student_ID, student_last_name, task1, task2, task3, task4) values (9, 'Sherstyuk', 'pass', 'not pass', 'not pass', 'not pass');
+insert into result (Student_ID, student_last_name, task1, task2, task3, task4) values (10, 'Kazimirov', 'pass', 'pass*', 'pass*', 'pass*');
+insert into result (Student_ID, student_last_name, task1, task2, task3, task4) values (11, 'Nazarov', 'pass', 'pass', 'pass', 'pass');
+insert into result (Student_ID, student_last_name, task1, task2, task3, task4) values (12, 'Shatrov', 'pass', 'pass*', 'pass', 'pass');
+insert into result (Student_ID, student_last_name, task1, task2, task3, task4) values (13, 'Nikolskiy', 'pass*', 'not pass', 'not pass', '');
+insert into result (Student_ID, student_last_name, task1, task2, task3, task4) values (14, 'Gurin', 'pass', 'pass', 'pass*', 'pass');
+insert into result (Student_ID, student_last_name, task1, task2, task3, task4) values (15, 'Kuzin', 'pass', 'not pass', 'not pass', '');
+insert into result (Student_ID, student_last_name, task1, task2, task3, task4) values (16, 'Nalivaiko', 'pass', 'pass', 'pass', '');
+insert into result (Student_ID, student_last_name, task1, task2, task3, task4) values (17, 'Chech', 'pass', 'pass', 'pass*', 'pass');
+insert into result (Student_ID, student_last_name, task1, task2, task3, task4) values (18, 'Yesman', 'pass', 'pass', 'pass*', 'pass*');
